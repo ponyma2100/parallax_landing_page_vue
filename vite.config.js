57d9c48dbb6a7b1370a289fb5e86d3f174522f05
PonyMa <ponyma2100@gmail.com>
2022-11-14 04:02:13 +0800

    Add config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import svgLoader from "vite-svg-loader";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader()],
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/<parallax_landing_page_vue>/"
+      : "./",
 });
